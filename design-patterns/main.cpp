3153b981ac4800e47f29d17e0ad6dff21a5a3551
王承权 <wangchengquan@qiyi.com>
2020-06-11 10:03:45 +0800

    Initial Commit

---
+++ design-patterns/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  design-patterns
+//
+//  Created by Steve on 2020/6/11.
+//  Copyright © 2020 Steve. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
